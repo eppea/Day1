bd7adbf8572ebb94f2e63ce20309aa15c0bc7f35
eppea <63557122+eppea@users.noreply.github.com>
2023-04-14 16:08:55 +0700

    main

--- DAY5/main.go
+++ DAY5/main.go
@@ -1,18 +1,23 @@
-ackage main
+package main
+
 import (
- "net/http"
- "github.com/labstack/echo"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
+
 func main() {
- // create a new echo instance
- e := echo.New()
- // Route / to handler function
- e.GET("/", HelloController)
- // start the server, and log if it fails
- e.Start(":8000")
+	// create a new echo instance
+	e := echo.New()
+	// Route / to handler function
+	e.GET("/", HelloController)
+	// start the server, and log if it fails
+	e.Start(":8000")
 }
+
 // handler - Simple handler to make sure environment is setup
 func HelloController(c echo.Context) error {
- // return the string "Hello World" as the response body
- // with an http.StatusOK (200) status
- return c.String(http.StatusOK, "Hello World")
+	// return the string "Hello World" as the response body
+	// with an http.StatusOK (200) status
+	return c.String(http.StatusOK, "Hello World")
+}
